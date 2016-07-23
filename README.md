777b71b7e55b7c6a6250bf0202313175c2316de4
fullautomedia <taryn@fullautomedia.com>
2016-07-22 23:56:36 -0600

    adding sqlite

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Ionic2 Beta10 Ionic-Native SQLite
 Demonstration on how to use Ionic2 Beta 10's ionic-native sqlite plugin
+
+1.) install Ionic2
+2.) ionic start MyProject --v2 --ts
+3.) cd MyProject
+4.) ionic platform add android
+6.) ionic plugin add cordova-sqlite-storage
+7.) import { SQLite } from 'ionic-native';
+8.) open sql db and create table
+9.) ionic run android -c -livereload
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,36 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import { SQLite } from 'ionic-native';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  message: string;
+
   constructor(private navCtrl: NavController) {
-  
+
+  }
+
+  ngOnInit() {
+    console.log("init");
+    
+    let db = new SQLite();
+    db.openDatabase({
+      name: 'data.db',
+      location: 'default' // the location field is required
+    }).then(() => {
+      this.message = "starting to create table";
+      db.executeSql('create table IF NOT EXISTS danceMoves(name VARCHAR(32))', {}).then(() => {
+        console.log("table created");
+        this.message = "table created";
+      }, (err) => {
+        console.error('Unable to execute sql', JSON.stringify(err));
+        this.message = 'Unable to execute sql', JSON.stringify(err);
+      })
+    }, (err) => {
+      console.error('Unable to open database', err);
+      this.message = 'Unable to open database';
+    });
   }
 }
