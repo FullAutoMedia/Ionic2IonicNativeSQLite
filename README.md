d15295b5ac9b1546ee0c40efbe2c0d17b724d5b9
fullautomedia <taryn@fullautomedia.com>
2016-07-23 13:33:13 -0600

    complete demo

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Ionic2 Beta10 Ionic-Native SQLite
 Demonstration on how to use Ionic2 Beta 10's ionic-native sqlite plugin
 
-1.) install Ionic2
+1.) Install <a href="http://ionicframework.com/docs/v2/getting-started/installation/">Ionic2</a>
 2.) ionic start MyProject --v2 --ts
 3.) cd MyProject
 4.) ionic platform add android
 6.) ionic plugin add cordova-sqlite-storage
 7.) import { SQLite } from 'ionic-native';
-8.) open sql db and create table
-9.) ionic run android -c -livereload
+8.) open sql db and create table, add and delete data
+9.) ionic run android -l -c
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -6,31 +6,47 @@ import { SQLite } from 'ionic-native';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  message: string;
+  private database: SQLite;
+  moves: Array<{ id: number, name: string }> = [];
 
-  constructor(private navCtrl: NavController) {
-
-  }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
-    console.log("init");
-    
-    let db = new SQLite();
-    db.openDatabase({
+    this.database = new SQLite();
+    this.database.openDatabase({
       name: 'data.db',
       location: 'default' // the location field is required
     }).then(() => {
-      this.message = "starting to create table";
-      db.executeSql('create table IF NOT EXISTS danceMoves(name VARCHAR(32))', {}).then(() => {
-        console.log("table created");
-        this.message = "table created";
-      }, (err) => {
-        console.error('Unable to execute sql', JSON.stringify(err));
-        this.message = 'Unable to execute sql', JSON.stringify(err);
-      })
-    }, (err) => {
-      console.error('Unable to open database', err);
-      this.message = 'Unable to open database';
+      this.database.executeSql('drop table danceMoves', []).then(() => {
+        this.database.executeSql('create table IF NOT EXISTS danceMoves(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32))', {}).then(() => {
+            this.refresh();
+          }, (err) => {
+            console.log('Unable to execute sql err:' + JSON.stringify(err));
+          })
+        }, (err) => {
+          console.log('Unable to opendb err:' + JSON.stringify(err));
+        });
+      });
+  }
+
+  public onAddClick(name: string) {
+    this.database.executeSql("INSERT INTO danceMoves(name) VALUES (?)", [name]).then(() => {
+      this.refresh();
+    });
+  }
+
+  public onDeleteClick(id: number) {
+    this.database.executeSql("DELETE FROM danceMoves WHERE id=?", [id]).then(() => {
+      this.refresh();
+    });
+  }
+
+  public refresh() {
+    this.database.executeSql("SELECT * FROM danceMoves", []).then((data) => {
+      this.moves = [];
+      for(let i = 0; i < data.rows.length; i++) {
+        this.moves.push({id: data.rows.item(i).id, name: data.rows.item(i).name});
+      }
     });
   }
 }
